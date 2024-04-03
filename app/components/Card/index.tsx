50fc5625e1b40cdd9f662352b5630313fc59f266
paanSinghCoder <gaurav3017@gmail.com>
2024-04-04 01:00:18 +0530

    feat(app): Add markers

--- app/components/Card/index.tsx
+++ app/components/Card/index.tsx
@@ -4,7 +4,7 @@ import { CardPropsType } from "./Card.types";
 const Card = ({ title = "", showMenu = false, children }: CardPropsType) => (
   <div className="rounded-lg border bg-white text-gray-500 font-semibold py-4 px-6 my-2">
     <nav className="flex w-full items-center justify-between">
-      <div>{title}</div>
+      <div className="pt-2">{title}</div>
       {showMenu ? (
         <Cog6ToothIcon className="h-6 w-6 text-slate-600 cursor-pointer" />
       ) : (
